102114cf0d44c642469142b7e9fc71d9af68282f
0x408 <github@0x408.dev>
2025-01-27 16:12:29 +0300

    chore: Remove requirement for a line break before and after the contents of h1-h6 singleline elements

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,6 +15,11 @@ export default withNuxt(
             "vue/html-indent": ["error", 4],
             "vue/max-attributes-per-line": ["error", { singleline: { max: 5 }, multiline: { max: 1 } }],
             "vue/multi-word-component-names": "off",
+            "vue/singleline-html-element-content-newline": ["error", {
+                ignoreWhenNoAttributes: true,
+                ignoreWhenEmpty: true,
+                externalIgnores: ["h1", "h2", "h3", "h4", "h5", "h6"]
+            }],
             "@stylistic/comma-dangle": ["error", "never"],
             "@stylistic/indent": ["error", 4],
             "@stylistic/quotes": ["error", "double"],
