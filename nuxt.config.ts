4160e93db6788b32577487d8b6c73a114faa0bb6
0x408 <github@0x408.dev>
2025-01-27 16:25:27 +0300

    chore: Add Tailwind CSS

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ["@nuxt/eslint", "@nuxt/fonts"],
+    modules: ["@nuxt/eslint", "@nuxt/fonts", "@nuxtjs/tailwindcss"],
     devtools: { enabled: true },
     compatibilityDate: "2024-11-01",
     eslint: {
