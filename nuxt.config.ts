a46ff967d3fcbef6998c332828db0b32698ef04f
0x408 <github@0x408.dev>
2025-01-28 20:38:29 +0300

    chore: Configure icon component directory as global

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     modules: ["@nuxt/eslint", "@nuxt/fonts", "@nuxtjs/tailwindcss", "@vueuse/nuxt"],
+    components: {
+        dirs: [
+            {
+                global: true,
+                path: "~/components/icon",
+                prefix: "Icon"
+            },
+            "~/components"
+        ]
+    },
     devtools: { enabled: true },
     compatibilityDate: "2024-11-01",
     eslint: {
